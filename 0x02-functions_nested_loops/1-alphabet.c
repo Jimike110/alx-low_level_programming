8fdea68c679c1ae5e13f8a2deded123c544583d1
Michael Oladoye <oladoyemike@gmail.com>
2022-09-13 07:28:55 -0700

    main.h

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+/**
+ * C program to print all alphabets from a to z
+ */
+
+#include <stdio.h>
+
+	int main = alphabet(void)
+{
+		char ch;
+
+		printf("Alphabets from a - z are: \n");
+		for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		printf(ch"\n");
+		}
+	/*
+	 * this returns value of 0
+	 */
+	return (0);
+	}
